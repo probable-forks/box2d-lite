575d51b527839a9ad85033a4867ffbc258ba8f6d
Erika <esweet431@gmail.com>
2020-08-26 16:52:45 -0700

    add unit tests

---
+++ tests/collide-test.cpp
@@ -0,0 +1,16 @@
+#include "gtest/gtest.h"
+
+TEST(CollisionTest, posA) {
+    EXPECT_EQ(1, 1);
+    EXPECT_TRUE(true);
+}
+
+TEST(CollisionTest, posB) {
+    EXPECT_EQ(1, 1);
+    EXPECT_TRUE(true);
+}
+
+TEST(CollisionTest, posC) {
+    EXPECT_EQ(1, 1);
+    EXPECT_TRUE(true);
+}
